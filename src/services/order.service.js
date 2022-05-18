d9a64015f319420cb4611ca9fa411a72e809a303
rishi dubey <rishi18454@gmail.com>
2022-05-18 23:49:38 +0530

    added createOrder service

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,6 +1,7 @@
 const httpStatus = require("http-status");
 const { Order } = require("../models");
 const ApiError = require("../utils/ApiError");
+const productService = require("./product.service");
 
 const getOrdersBySellerId = async (seller_id) => {
   const orders = await Order.find({ seller: seller_id });
@@ -9,6 +10,25 @@ const getOrdersBySellerId = async (seller_id) => {
 
 //create order from a list of products,
 //items array contains a list of product ids
-const createOrder = async (items) => {};
+const createOrder = async (buyer, seller, items) => {
+  const products = await Promise.all(
+    items.map(async (item) => {
+      const product = await productService.getProductById(item);
 
-module.exports = { getOrdersBySellerId };
+      //if an item is detected which is not sold by the seller, throw
+      if (!product)
+        throw new ApiError(
+          httpStatus.BAD_REQUEST,
+          `seller does not provide item with id ${item}`
+        );
+
+      return product;
+    })
+  );
+
+  const order = new Order({ buyer, seller, items });
+  await order.save();
+  return order;
+};
+
+module.exports = { createOrder, getOrdersBySellerId };
